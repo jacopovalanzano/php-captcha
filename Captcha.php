aecd387f2ef33d9c3e291dd8d2eac527d260856d
jacopovalanzano <jacopo.valanzano@gmail.com>
2021-06-21 01:14:50 +0100

    inital commit

---
+++ Captcha.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ * A simple and portable captcha.
+ *
+ * @author Jacopo Valanzano <jacopo.valanzano@gmail.com>
+ *
+ * @copyright MIT anonymous
+ */
+class Captcha
+{
+
+    /**
+     * The current captcha image instance.
+     */
+    private $captcha;
+
+    /**
+     * The captcha passphrase.
+     *
+     * @var string
+     */
+    private $passphrase;
+
+    /**
+     * The ttf fonts list.
+     *
+     * @var array
+     */
+    private $fonts;
+
+    /**
+     * Quantity of lines to display in front of the text layer.
+     *
+     * @var int
+     */
+    private $linesFront;
+
+    /**
+     * Quantity of lines to display behind the text layer.
+     *
+     * @var int
+     */
+    private $linesBack;
+
+    /**
+     * Captcha constructor.
+     *
+     * @param $passphrase
+     * @param array $fonts Full path of additional fonts.
+     */
+    public function __construct($passphrase, array $fonts = [])
+    {
+        $this->passphrase = is_array($passphrase) ? implode(" ", $passphrase) : $passphrase;
+
+        $path = realpath(dirname(__FILE__));
+
+        $this->fonts = array_merge([
+            "${path}/Fonts/Raleway-Black.ttf",
+            "${path}/Fonts/OpenSans-Bold.ttf",
+            "${path}/Fonts/Roboto-BlackItalic.ttf"
+        ], $fonts);
+    }
+
+    /**
+     * Creates a new captcha.
+     *
+     * @param int $imgWidth
+     * @param int $imgHeight
+     * @param int $fontSize
+     */
+    public function build($imgWidth = 175, $imgHeight = 50, $fontSize = 18)
+    {
+        $this->createImage($imgWidth, $imgHeight, $fontSize);
+    }
+
+    /**
+     * Outputs a created captcha, including headers.
+     */
+    public function out()
+    {
+        $this->getCaptcha();
+    }
+
+    /**
+     * Creates a new captcha.
+     *
+     * @param int $imgWidth
+     * @param int $imgHeight
+     * @param int $fontSize
+     */
+    public function createImage($imgWidth = 175, $imgHeight = 50, $fontSize = 18)
+    {
+        // Initialize an image
+        $this->captcha = imagecreatetruecolor($imgWidth, $imgHeight);
+
+        // Select a random color and create a rectangle
+        $rcolor = imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255));
+        $rectangle = imagefilledrectangle($this->captcha, 0, 0, $imgWidth - 1, $imgHeight - 1, $rcolor);
+
+        // Select one more random color for our letters
+        $rcolor = imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255));
+
+        // Create the text bounding (distances to fit container)
+        $textBox = imagettfbbox($fontSize, $rectangle, $this->fonts[rand(0, count($this->fonts)-1)], $this->passphrase);
+
+        $textWidth = abs(max($textBox[2], $textBox[4]));
+
+        $textHeight = abs(max($textBox[5], $textBox[7]));
+
+        $x = (imagesx($this->captcha) - $textWidth) / 2;
+        $y = (imagesy($this->captcha) + $textHeight) / 2;
+
+        // Creates lines behind the string.
+        for($i=0;$i<$this->linesBack;$i++) {
+            imageline($this->captcha, rand(0, $imgWidth), rand(0, $imgHeight), rand(0, $imgWidth), rand(0, $imgHeight), imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+        }
+
+        // Add shadow
+        imagettftext($this->captcha, $fontSize, $rectangle, $x+rand(-5,5), $y+rand(-5,5), imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)), $this->fonts[rand(0,count($this->fonts)-1)], $this->passphrase);
+
+        //add the text
+        imagettftext($this->captcha, $fontSize, $rectangle, $x, $y, $rcolor, $this->fonts[rand(0,count($this->fonts)-1)], $this->passphrase);
+
+        // Creates lines that overlap the string.
+        for($i=0;$i<$this->linesFront;$i++) {
+            imageline($this->captcha, rand(0, $imgWidth), rand(0, $imgHeight), rand(0, $imgWidth), rand(0, $imgHeight), imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+        }
+
+        $this->captcha = $this->distort($this->captcha, $imgWidth, $imgHeight, imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+    }
+
+    /**
+     * Creates a captcha with PHP default fonts.
+     * You should use Captcha::createImage() when possible.
+     *
+     * @param int $imgWidth
+     * @param int $imgHeight
+     */
+    public function createStringImage($imgWidth = 175, $imgHeight = 50)
+    {
+
+        // Initialize an image
+        $this->captcha = imagecreatetruecolor($imgWidth, $imgHeight);
+
+        // Select a random color and create a rectangle
+        $rcolor = imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255));
+        imagefilledrectangle($this->captcha, 0, 0, $imgWidth - 1, $imgHeight - 1, $rcolor);
+
+        // Creates lines behind the string.
+        for($i=0;$i<$this->linesBack;$i++) {
+            imageline($this->captcha, rand(0, $imgWidth), rand(0, $imgHeight), rand(0, $imgWidth), rand(0, $imgHeight), imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+        }
+
+        // Calculate x and y coordinates
+        $x = rand(0, (imagesx($this->captcha)) - $imgWidth / 2);
+        $y = rand(0, (imagesy($this->captcha)) - $imgHeight / 2);
+
+        // Creates the shadow of the string.
+        imagestring($this->captcha, 4, $x, $y, $this->passphrase, imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+
+        // Creates another string ±3px on top of the "shadow".
+        imagestring($this->captcha, 5, $x+rand(-1,1), $y+rand(-1,1), $this->passphrase, imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+
+        // Creates lines that overlap the string.
+        for($i=0;$i<$this->linesFront;$i++) {
+            imageline($this->captcha, rand(0, $imgWidth), rand(0, $imgHeight), rand(0, $imgWidth), rand(0, $imgHeight), imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+        }
+
+        $this->captcha = $this->distort($this->captcha, $imgWidth, $imgHeight, imagecolorallocate($this->captcha, rand(0,255), rand(0,255), rand(0,255)));
+    }
+
+    /**
+     * For generating random lines that overlap the text.
+     *
+     * @param $lines Number of lines to create.
+     * @return $this
+     */
+    public function linesFront($lines)
+    {
+        $this->linesFront = $lines;
+        return $this;
+    }
+
+    /**
+     * For generating random lines behind the text.
+     *
+     * @param $lines Number of lines to create.
+     * @return $this
+     */
+    public function linesBack($lines)
+    {
+        $this->linesBack = $lines;
+        return $this;
+    }
+
+    /**
+     * Returns the captcha passphrase
+     *
+     * @return string
+     */
+    public function getPassphrase()
+    {
+        return $this->passphrase;
+    }
+
+    /**
+     * Renders the captcha image.
+     *
+     * @param int $quality Captcha image quality.
+     */
+    public function getCaptcha($quality = 100)
+    {
+        header('Content-Type: image/jpeg');
+        imagejpeg($this->captcha, null, $quality);
+        imagedestroy($this->captcha);
+    }
+
+    /**
+     * Image distortion
+     */
+    protected function distort($image, $width, $height, $bg)
+    {
+        $contents = imagecreatetruecolor($width, $height);
+        $X          = rand(0, $width);
+        $Y          = rand(0, $height);
+        $phase      = rand(0, 10);
+        $scale      = 1.1 + rand(0, 10000) / 30000;
+        for ($x = 0; $x < $width; $x++) {
+            for ($y = 0; $y < $height; $y++) {
+                $Vx = $x - $X;
+                $Vy = $y - $Y;
+                $Vn = sqrt($Vx * $Vx + $Vy * $Vy);
+
+                if ($Vn != 0) {
+                    $Vn2 = $Vn + 4 * sin($Vn / 30);
+                    $nX  = $X + ($Vx * $Vn2 / $Vn);
+                    $nY  = $Y + ($Vy * $Vn2 / $Vn);
+                } else {
+                    $nX = $X;
+                    $nY = $Y;
+                }
+                $nY = $nY + $scale * sin($phase + $nX * 0.2);
+
+                $p = $this->getCol($image, round($nX), round($nY), $bg);
+
+                if ($p == 0) {
+                    $p = $bg;
+                }
+
+                imagesetpixel($contents, $x, $y, $p);
+            }
+        }
+
+        return $contents;
+    }
+
+    /**
+     * @param $image
+     * @param $x
+     * @param $y
+     *
+     * @return int
+     */
+    protected function getCol($image, $x, $y, $background)
+    {
+        $L = imagesx($image);
+        $H = imagesy($image);
+        if ($x < 0 || $x >= $L || $y < 0 || $y >= $H) {
+            return $background;
+        }
+
+        return imagecolorat($image, $x, $y);
+    }
+}
---
+++ README.md
@@ -0,0 +1,82 @@
+# PHP Captcha
+
+Portable PHP class to create simple captchas.
+
+## Installation
+
+You can use composer, or simply copy/paste the **Captcha** class.
+
+```bash
+composer require jacopovalanzano/php-captcha
+```
+
+## Usage
+Since the captcha is a binary jpeg image, it can be rendered with the "image/jpeg" content-type header;
+```php
+    // Create a new Captcha
+    $captcha = new Captcha("My super difficult to read string.");
+
+    // Add 3 lines over and 3 behind the text,
+    // then build the image.
+    $captcha->addLinesFront(3)->addLinesBack(3)->build(175,50); // width, height
+
+    // Returns a string containing the captcha passphrase
+    $captcha->getPassphrase(); // Returns "My super difficult to read string."
+
+    // Renders the captcha.
+    $captcha->out();
+
+```
+
+## Example
+A simple example to explain the process of dispatching/retrieving a captcha and its passphrase:
+
+```php
+    // This file represents the "www.example.com/get_captcha_image" url
+ 
+    // ...    
+
+    // By default, the captcha class returns 2 random words
+    // from the list provided.
+    $captcha = new Captcha();
+
+    // Add 2 lines over and 5 behind the text,
+    // then build the image.
+    $captcha->addLinesFront(2)->addLinesBack(5)->build(175,50); // width, height
+
+    // Save the captcha to session, so it can be retrieved later... 
+    $_SESSION["captcha_passphrase"] = $captcha->getPassphrase();
+
+    // Render the actual captcha image
+    $captcha->out();
+```
+
+An example of a form you need to validate, like a login form:
+
+```html
+    <!-- ...  -->
+
+<input type="email" name="email">
+<input type="password" name="password">
+
+<!-- Render the actual captcha image using the URL of our captcha-generator (see above): -->
+<img id="captcha_image" src="www.example.com/get_captcha_image" alt="captcha">
+<input type="text" name="captcha_passphrase">
+<input type="submit" value="send">
+```
+
+To validate a request from such a login form, one would simply match ```$_SESSION["captcha_passphrase"]```
+against the value passed from the input "captcha_passphrase" in the example above, eg:
+
+```php
+     // Compare the captcha passphrase with the one submitted
+    if($_POST["captcha_passphrase"] !== $_SESSION["captcha_passphrase"]) {
+        die("The captchas do not match!");
+    }
+```
+
+## Contributing
+Pull requests are welcome.
+
+## License
+[MIT]()
